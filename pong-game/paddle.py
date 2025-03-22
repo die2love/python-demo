260bf9799b6fc867544dca9081577cc36e3dd76f
die2love <68505548+die2love@users.noreply.github.com>
2025-03-22 22:41:26 +0530

    first commit

---
+++ pong-game/paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self, positiom):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(positiom)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
